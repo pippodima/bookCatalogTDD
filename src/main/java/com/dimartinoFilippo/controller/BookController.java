6c694e4396a07565a1dee519b15130bd6bfb2ffb
pippodima <pippodima99@gmail.com>
2026-05-13 16:29:43 +0200

    implemented findAllBooks green test

--- src/main/java/com/dimartinoFilippo/controller/BookController.java
+++ src/main/java/com/dimartinoFilippo/controller/BookController.java
@@ -1,10 +1,23 @@
 package com.dimartinoFilippo.controller;
 
+import com.dimartinoFilippo.repository.BookRepository;
+import com.dimartinoFilippo.view.LibraryView;
+
 public class BookController {
+	
+	private BookRepository bookRepository;
+	private LibraryView libraryView;
+
+	public BookController(BookRepository bookRepository, LibraryView libraryView) {
+		this.bookRepository = bookRepository;
+		this.libraryView = libraryView;
+	}
+
+
+
 
 	public void findAllBooks() {
-		// TODO Auto-generated method stub
-		
+		libraryView.showAllBooks(bookRepository.findAll());		
 	}
 
 }
