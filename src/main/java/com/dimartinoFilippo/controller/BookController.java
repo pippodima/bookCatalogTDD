bb2c8efd020f14aa890404b419ee8db788709c5b
pippodima <pippodima99@gmail.com>
2026-05-14 17:30:17 +0200

    testAddNewBookWhenAuthorDoesNotExist green

--- src/main/java/com/dimartinoFilippo/controller/BookController.java
+++ src/main/java/com/dimartinoFilippo/controller/BookController.java
@@ -1,16 +1,20 @@
 package com.dimartinoFilippo.controller;
 
+import com.dimartinoFilippo.model.Author;
 import com.dimartinoFilippo.model.Book;
+import com.dimartinoFilippo.repository.AuthorRepository;
 import com.dimartinoFilippo.repository.BookRepository;
 import com.dimartinoFilippo.view.LibraryView;
 
 public class BookController {
 	
 	private BookRepository bookRepository;
+	private AuthorRepository authorRepository;
 	private LibraryView libraryView;
 
-	public BookController(BookRepository bookRepository, LibraryView libraryView) {
+	public BookController(BookRepository bookRepository, AuthorRepository authorRepository, LibraryView libraryView) {
 		this.bookRepository = bookRepository;
+		this.authorRepository = authorRepository;
 		this.libraryView = libraryView;
 	}
 
@@ -28,6 +32,11 @@ public class BookController {
 		Book existingBook = bookRepository.findByIsbn(bookToAdd.getIsbn());
 		if (existingBook != null) {
 			libraryView.showErrorBookAlreadyExists("The selected ISBN " + bookToAdd.getIsbn() + " is already in use", existingBook);
+			return;
+		}
+		Author existingAuthor = authorRepository.findById(bookToAdd.getAuthor().getId());
+		if (existingAuthor == null) {
+			libraryView.showErrorAuthorDoesNotExist("The selected author does not exist", bookToAdd.getAuthor());
 			return;
 		}
 		bookRepository.save(bookToAdd);
