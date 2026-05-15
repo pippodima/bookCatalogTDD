548c98a8daee0aacbe5c1e16093a58f24283e350
pippodima <pippodima99@gmail.com>
2026-05-15 14:57:19 +0200

    test testFindAllAuthors green

--- src/main/java/com/dimartinoFilippo/controller/AuthorController.java
+++ src/main/java/com/dimartinoFilippo/controller/AuthorController.java
@@ -1,9 +1,21 @@
 package com.dimartinoFilippo.controller;
 
+import com.dimartinoFilippo.repository.AuthorRepository;
+import com.dimartinoFilippo.view.LibraryView;
+
 public class AuthorController {
+	
+	private LibraryView libraryView;
+	
+	private AuthorRepository authorRepository;
+	
+	public AuthorController(LibraryView libraryView, AuthorRepository authorRepository) {
+		this.libraryView = libraryView;
+		this.authorRepository = authorRepository;
+	}
 
 	public void findAllAuthors() {
-		// TODO Auto-generated method stub
+		libraryView.showAllAuthors(authorRepository.findAll());
 		
 	}
 
