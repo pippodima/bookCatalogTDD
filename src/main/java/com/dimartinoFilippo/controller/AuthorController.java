63e19a940e211cae69392eaf7dc0e0a2110cafbf
pippodima <pippodima99@gmail.com>
2026-05-18 16:29:24 +0200

    testDeleteAuthorWhenExistAndThereIsOneRelatedBook green test

--- src/main/java/com/dimartinoFilippo/controller/AuthorController.java
+++ src/main/java/com/dimartinoFilippo/controller/AuthorController.java
@@ -1,7 +1,11 @@
 package com.dimartinoFilippo.controller;
 
+import java.util.List;
+
 import com.dimartinoFilippo.model.Author;
+import com.dimartinoFilippo.model.Book;
 import com.dimartinoFilippo.repository.AuthorRepository;
+import com.dimartinoFilippo.repository.BookRepository;
 import com.dimartinoFilippo.view.LibraryView;
 
 public class AuthorController {
@@ -10,9 +14,12 @@ public class AuthorController {
 	
 	private AuthorRepository authorRepository;
 	
-	public AuthorController(LibraryView libraryView, AuthorRepository authorRepository) {
+	private BookRepository bookRepository;
+	
+	public AuthorController(LibraryView libraryView, AuthorRepository authorRepository, BookRepository bookRepository) {
 		this.libraryView = libraryView;
 		this.authorRepository = authorRepository;
+		this.bookRepository = bookRepository;
 	}
 
 	public void findAllAuthors() {
@@ -32,6 +39,11 @@ public class AuthorController {
 	}
 
 	public void deleteAuthor(Author authorToDelete) {
+		List<Book> authorsBooks = bookRepository.findByAuthor(authorToDelete.getId());
+		for (Book book : authorsBooks) {
+			bookRepository.delete(book);
+			libraryView.bookRemoved(book);
+		}
 		authorRepository.delete(authorToDelete);
 		libraryView.authorRemoved(authorToDelete);
 		
