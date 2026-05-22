649d82e54dec704f81cd9908db1a5aa8d4241add
pippodima <pippodima99@gmail.com>
2026-05-22 16:03:04 +0200

    added IT find tests

--- bookCatalog/src/it/java/com/dimartinoFilippo/repository/mongo/AuthorMongoRepositoryTestcontainersIT.java
+++ bookCatalog/src/it/java/com/dimartinoFilippo/repository/mongo/AuthorMongoRepositoryTestcontainersIT.java
@@ -2,10 +2,13 @@ package com.dimartinoFilippo.repository.mongo;
 
 import static com.dimartinoFilippo.repository.mongo.BookMongoRepository.DB_NAME;
 import static com.dimartinoFilippo.repository.mongo.AuthorMongoRepository.AUTHOR;
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.bson.Document;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.ClassRule;
+import org.junit.Test;
 import org.testcontainers.containers.MongoDBContainer;
 import org.testcontainers.utility.DockerImageName;
 
@@ -44,6 +47,29 @@ public class AuthorMongoRepositoryTestcontainersIT {
 	public void tearDown() {
 		client.close();
 	}
+	
+	@Test
+	public void testITFindAll() {
+		addTestAuthorToDatabase(TEST_AUTHOR_1);
+		addTestAuthorToDatabase(TEST_AUTHOR_2);
+		assertThat(authorRepository.findAll()).containsExactly(TEST_AUTHOR_1, TEST_AUTHOR_2);
+	}
+
+	@Test
+	public void testITFindById() {
+		addTestAuthorToDatabase(TEST_AUTHOR_1);
+		addTestAuthorToDatabase(TEST_AUTHOR_2);
+		assertThat(authorRepository.findById("a2")).isEqualTo(TEST_AUTHOR_2);
+	}
+	
+	
+	private void addTestAuthorToDatabase(Author author) {
+		collection.insertOne(new Document()
+				.append("id", author.getId())
+				.append("firstName", author.getFirstName())
+				.append("lastName", author.getLastName()));
+	}
+
 
 
 }
