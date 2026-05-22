2264409b209be111e1ce8336c6436a389e9b7ac1
pippodima <pippodima99@gmail.com>
2026-05-22 14:51:37 +0200

    correct first implemetations AuthorMongoRepository(Test)

--- bookCatalog/src/main/java/com/dimartinoFilippo/repository/mongo/AuthorMongoRepository.java
+++ bookCatalog/src/main/java/com/dimartinoFilippo/repository/mongo/AuthorMongoRepository.java
@@ -1,11 +1,28 @@
 package com.dimartinoFilippo.repository.mongo;
 
+import static com.dimartinoFilippo.repository.mongo.BookMongoRepository.DB_NAME;
+
 import java.util.List;
+
+import org.bson.Document;
 
 import com.dimartinoFilippo.model.Author;
 import com.dimartinoFilippo.repository.AuthorRepository;
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoCollection;
 
 public class AuthorMongoRepository implements AuthorRepository{
+	
+	public static final String AUTHOR = "authors";
+
+	private MongoCollection<Document> collection;
+	
+	public AuthorMongoRepository (MongoClient client) {
+		collection = client
+				.getDatabase(DB_NAME)
+				.getCollection(AUTHOR);
+	}
+
 
 	@Override
 	public Author findById(String id) {
--- bookCatalog/src/test/java/com/dimartinoFilippo/repository/mongo/AuthorMongoRepositoryTest.java
+++ bookCatalog/src/test/java/com/dimartinoFilippo/repository/mongo/AuthorMongoRepositoryTest.java
@@ -1,6 +1,6 @@
 package com.dimartinoFilippo.repository.mongo;
 
-import static com.dimartinoFilippo.repository.mongo.BookMongoRepository.BOOK;
+import static com.dimartinoFilippo.repository.mongo.AuthorMongoRepository.AUTHOR;
 import static com.dimartinoFilippo.repository.mongo.BookMongoRepository.DB_NAME;
 import static org.junit.Assert.*;
 
@@ -14,7 +14,6 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 
 import com.dimartinoFilippo.model.Author;
-import com.dimartinoFilippo.model.Book;
 import com.mongodb.MongoClient;
 import com.mongodb.ServerAddress;
 import com.mongodb.client.MongoCollection;
@@ -25,46 +24,40 @@ import de.bwaldvogel.mongo.backend.memory.MemoryBackend;
 
 public class AuthorMongoRepositoryTest {
 	
-	private static final Author TEST_AUTHOR = new Author("a1", "Italo", "Calvino");
-	private static final Book TEST_BOOK_1 = new Book("1234", "Il Barone Rampante", TEST_AUTHOR, 1957);
-	private static final Book TEST_BOOK_2 = new Book("12345", "Il Visconte Dimezzato", TEST_AUTHOR, 1952);
+    private static final Author TEST_AUTHOR_1 = new Author("a1", "Italo", "Calvino");
+    private static final Author TEST_AUTHOR_2 = new Author("a2", "Umberto", "Eco");
 
 	private static MongoServer server;
 	private static InetSocketAddress serverAddress;
 	
-	private MongoClient client;
-	private BookMongoRepository bookRepository;
-	private MongoCollection<Document> collection;
+    private MongoClient client;
+    private AuthorMongoRepository authorRepository;
+    private MongoCollection<Document> collection;
 	
-	@BeforeClass
-	public static void setupServer() {
-		server = new MongoServer(new MemoryBackend());
-		serverAddress = server.bind();
-	}
-	
-	@AfterClass
-	public static void shutDownServer() {
-		server.shutdown();
-	}
+    @BeforeClass
+    public static void setupServer() {
+        server = new MongoServer(new MemoryBackend());
+        serverAddress = server.bind();
+    }
 
-	@Before
-	public void setup() {
-		client = new MongoClient(new ServerAddress(serverAddress));
-		bookRepository = new BookMongoRepository(client);
-		MongoDatabase database = client.getDatabase(DB_NAME);
-		database.drop();
-		collection = database.getCollection(BOOK);
-	}
-	
-	@After
-	public void tearDown() {
-		client.close();
-	}
+    @AfterClass
+    public static void shutDownServer() {
+        server.shutdown();
+    }
 
+    @Before
+    public void setup() {
+        client = new MongoClient(new ServerAddress(serverAddress));
+        authorRepository = new AuthorMongoRepository(client);
+        MongoDatabase database = client.getDatabase(DB_NAME);
+        database.drop();
+        collection = database.getCollection(AUTHOR);
+    }
 	
-	@Test
-	public void test() {
-		fail("Not yet implemented");
-	}
+    @After
+    public void tearDown() {
+        client.close();
+    }
+
 
 }
