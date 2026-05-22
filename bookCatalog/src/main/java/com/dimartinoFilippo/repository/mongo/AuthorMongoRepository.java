91f1023a7b5091182fcb8049580431083fe7c966
pippodima <pippodima99@gmail.com>
2026-05-22 14:43:27 +0200

    created AuthorMongoRepository and Test file

---
+++ bookCatalog/src/main/java/com/dimartinoFilippo/repository/mongo/AuthorMongoRepository.java
@@ -0,0 +1,34 @@
+package com.dimartinoFilippo.repository.mongo;
+
+import java.util.List;
+
+import com.dimartinoFilippo.model.Author;
+import com.dimartinoFilippo.repository.AuthorRepository;
+
+public class AuthorMongoRepository implements AuthorRepository{
+
+	@Override
+	public Author findById(String id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Author> findAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void save(Author newAuthor) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void delete(Author authorToDelete) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
