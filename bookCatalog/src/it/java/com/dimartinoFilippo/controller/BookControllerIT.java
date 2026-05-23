2f8f03935a0c0b29027665d2816a2a57322e47f2
pippodima <pippodima99@gmail.com>
2026-05-23 19:11:04 +0200

    created BookControllerIT

---
+++ bookCatalog/src/it/java/com/dimartinoFilippo/controller/BookControllerIT.java
@@ -0,0 +1,61 @@
+package com.dimartinoFilippo.controller;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.ClassRule;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.testcontainers.containers.MongoDBContainer;
+import org.testcontainers.utility.DockerImageName;
+
+import com.dimartinoFilippo.model.Author;
+import com.dimartinoFilippo.model.Book;
+import com.dimartinoFilippo.repository.mongo.AuthorMongoRepository;
+import com.dimartinoFilippo.repository.mongo.BookMongoRepository;
+import com.dimartinoFilippo.view.LibraryView;
+import com.mongodb.MongoClient;
+import com.mongodb.ServerAddress;
+
+public class BookControllerIT {
+	
+	private static final Author TEST_AUTHOR = new Author("a1", "Italo", "Calvino");
+	private static final Book TEST_BOOK_1 = new Book("1234", "Il Barone Rampante", TEST_AUTHOR, 1957);
+	private static final Book TEST_BOOK_2 = new Book("12345", "Il Visconte Dimezzato", TEST_AUTHOR, 1952);
+
+	@ClassRule
+	public static final MongoDBContainer mongo =
+	new MongoDBContainer(DockerImageName.parse("mongo:4.0.5"));
+
+	@Mock
+	LibraryView libraryView;
+	
+	private BookMongoRepository bookRepository;
+	private AuthorMongoRepository authorRepository;
+	private BookController bookController;
+	private AutoCloseable closeable;
+	
+	@Before
+	public void setUp() {
+		closeable = MockitoAnnotations.openMocks(this);
+		MongoClient client = new MongoClient(
+				new ServerAddress(mongo.getHost(), mongo.getFirstMappedPort()));
+		bookRepository = new BookMongoRepository(client);
+		authorRepository = new AuthorMongoRepository(client);
+		
+		for (Book book : bookRepository.findAll()) {
+			bookRepository.delete(book.getIsbn());
+		}
+		
+		for (Author author : authorRepository.findAll()) {
+			authorRepository.delete(author.getId());
+		}
+		bookController = new BookController(bookRepository, authorRepository, libraryView);
+	}
+	
+	@After
+	public void releaseMocks()throws Exception {
+		closeable.close();
+	}
+	
+
+}
