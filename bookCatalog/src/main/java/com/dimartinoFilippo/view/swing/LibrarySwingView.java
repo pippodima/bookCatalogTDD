ba455901b02352fd180c0ec71c6117f64ca97b65
pippodima <pippodima99@gmail.com>
2026-05-25 13:31:13 +0200

    window title

--- bookCatalog/src/main/java/com/dimartinoFilippo/view/swing/LibrarySwingView.java
+++ bookCatalog/src/main/java/com/dimartinoFilippo/view/swing/LibrarySwingView.java
@@ -36,6 +36,7 @@ public class LibrarySwingView extends JFrame implements LibraryView{
 	 * Create the frame.
 	 */
 	public LibrarySwingView() {
+		setTitle("Library View");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
