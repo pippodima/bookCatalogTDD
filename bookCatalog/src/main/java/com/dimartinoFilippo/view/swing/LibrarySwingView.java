47d0559031228178fec7f54415bb5bec5624a1c6
pippodima <pippodima99@gmail.com>
2026-05-25 12:00:43 +0200

    added LibrarySwingView with test suite

---
+++ bookCatalog/src/main/java/com/dimartinoFilippo/view/swing/LibrarySwingView.java
@@ -0,0 +1,107 @@
+package com.dimartinoFilippo.view.swing;
+
+import java.awt.EventQueue;
+import java.util.List;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+import com.dimartinoFilippo.model.Author;
+import com.dimartinoFilippo.model.Book;
+import com.dimartinoFilippo.view.LibraryView;
+
+public class LibrarySwingView extends JFrame implements LibraryView{
+
+	private static final long serialVersionUID = 1L;
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					LibrarySwingView frame = new LibrarySwingView();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public LibrarySwingView() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+
+	}
+
+	@Override
+	public void showAllBooks(List<Book> books) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void newBookAdded(Book bookToAdd) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void showErrorBookAlreadyExists(String errorMessage, Book existingBook) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void showErrorAuthorDoesNotExist(String errorMessage, Author notExistigAuthor) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void bookRemoved(Book bookToRemove) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void showErrorBookDoesNotExist(String errorMessage, Book notExistingBook) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void showAllAuthors(List<Author> authors) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void newAuthorAdded(Author authorToAdd) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void showErrorAuthorAlreadyExist(String errorMessage, Author existingAuthor) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void authorRemoved(Author authorToRemove) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ bookCatalog/src/test/java/com/dimartinoFilippo/view/swing/LibrarySwingViewTest.java
@@ -0,0 +1,14 @@
+package com.dimartinoFilippo.view.swing;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class LibrarySwingViewTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
