8ab0253892aaefafb4bbeda023c343b4294397e2
pippodima <pippodima99@gmail.com>
2026-05-26 15:53:23 +0200

    moved main to app

---
+++ bookCatalog/src/main/java/com/dimartinoFilippo/app/LibrarySwingApp.java
@@ -0,0 +1,29 @@
+package com.dimartinoFilippo.app;
+
+import java.awt.EventQueue;
+import com.dimartinoFilippo.controller.AuthorController;
+import com.dimartinoFilippo.controller.BookController;
+import com.dimartinoFilippo.repository.mongo.AuthorMongoRepository;
+import com.dimartinoFilippo.repository.mongo.BookMongoRepository;
+import com.dimartinoFilippo.view.swing.LibrarySwingView;
+import com.mongodb.MongoClient;
+
+public class LibrarySwingApp {
+
+    public static void main(String[] args) {
+        MongoClient client = new MongoClient("localhost");
+        BookMongoRepository bookRepository = new BookMongoRepository(client);
+        AuthorMongoRepository authorRepository = new AuthorMongoRepository(client);
+
+        EventQueue.invokeLater(() -> {
+            LibrarySwingView view = new LibrarySwingView();
+            BookController bookController =
+                new BookController(bookRepository, authorRepository, view);
+            AuthorController authorController =
+                new AuthorController(view, authorRepository, bookRepository);
+            view.setBookController(bookController);
+            view.setAuthorController(authorController);
+            view.setVisible(true);
+        });
+    }
+}
