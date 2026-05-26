bec9783d8bf382fa2ae6ad592cccbd7d5e49513d
pippodima <pippodima99@gmail.com>
2026-05-26 15:43:38 +0200

    moved main to app class

--- bookCatalog/src/main/java/com/dimartinoFilippo/view/swing/LibrarySwingView.java
+++ bookCatalog/src/main/java/com/dimartinoFilippo/view/swing/LibrarySwingView.java
@@ -55,22 +55,6 @@ public class LibrarySwingView extends JFrame implements LibraryView{
 	private JButton btnDeleteBook;
 	private JLabel lblErrorBook;
 
-	
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					LibrarySwingView frame = new LibrarySwingView();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
 
 	/**
 	 * Create the frame.
