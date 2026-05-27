ae64936ec2847bfaef82828f2556e1b77d98a6f4
pippodima <pippodima99@gmail.com>
2026-05-27 17:15:54 +0200

    added picocli to main app

--- bookCatalog/src/main/java/com/dimartinoFilippo/app/LibrarySwingApp.java
+++ bookCatalog/src/main/java/com/dimartinoFilippo/app/LibrarySwingApp.java
@@ -1,6 +1,11 @@
 package com.dimartinoFilippo.app;
 
 import java.awt.EventQueue;
+import java.util.concurrent.Callable;
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+
 import com.dimartinoFilippo.controller.AuthorController;
 import com.dimartinoFilippo.controller.BookController;
 import com.dimartinoFilippo.repository.mongo.AuthorMongoRepository;
@@ -9,28 +14,40 @@ import com.dimartinoFilippo.view.swing.LibrarySwingView;
 import com.mongodb.MongoClient;
 import com.mongodb.ServerAddress;
 
-public class LibrarySwingApp {
+@Command(mixinStandardHelpOptions = true)
+public class LibrarySwingApp implements Callable<Void> {
+
+	@Option(names = {"--mongo-host"}, description = "MongoDB host address")
+	private String mongoHost = "localhost";
+
+	@Option(names = {"--mongo-port"}, description = "MongoDB host port")
+	private int mongoPort = 27017;
 
 	public static void main(String[] args) {
-		String host = System.getProperty("mongo.host", "localhost");
-		int port = Integer.parseInt(
-				System.getProperty("mongo.port", "27017"));
+		new CommandLine(new LibrarySwingApp()).execute(args);
+	}
 
-		MongoClient client = new MongoClient(new ServerAddress(host, port));
-		BookMongoRepository bookRepository = new BookMongoRepository(client);
-		AuthorMongoRepository authorRepository = new AuthorMongoRepository(client);
-
+	@Override
+	public Void call() throws Exception {
 		EventQueue.invokeLater(() -> {
-			LibrarySwingView view = new LibrarySwingView();
-			BookController bookController =
-					new BookController(bookRepository, authorRepository, view);
-			AuthorController authorController =
-					new AuthorController(view, authorRepository, bookRepository);
-			view.setBookController(bookController);
-			view.setAuthorController(authorController);
-			view.setVisible(true);
-			
-			authorController.findAllAuthors();
-			bookController.findAllBooks();
+			try {
+				MongoClient client = new MongoClient(new ServerAddress(mongoHost, mongoPort));
+				AuthorMongoRepository authorRepository = new AuthorMongoRepository(client);
+				BookMongoRepository bookRepository = new BookMongoRepository(client);
+				LibrarySwingView view = new LibrarySwingView();
+				BookController bookController =
+						new BookController(bookRepository, authorRepository, view);
+				AuthorController authorController =
+						new AuthorController(view, authorRepository, bookRepository);
+				view.setBookController(bookController);
+				view.setAuthorController(authorController);
+				view.setVisible(true);
+				authorController.findAllAuthors();
+				bookController.findAllBooks();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
 		});
-	}}
+		return null;
+	}
+}
